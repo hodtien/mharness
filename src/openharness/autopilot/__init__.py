379b7fc50e51ff25156e650f77c9b89caeba1ee0
Tien Dinh <hodtienbrian@gmail.com>
2026-05-09 18:07:41 +0700

    Autopilot: [P13.1] Backend: Pre-flight check trước khi run card (#118)
    * autopilot(ap-9531937c): [P13.1] Backend: Pre-flight check trước khi run card
    
    * autopilot(ap-9531937c): [P13.1] Backend: Pre-flight check trước khi run card
    
    ---------
    
    Co-authored-by: hodtien

--- src/openharness/autopilot/__init__.py
+++ src/openharness/autopilot/__init__.py
@@ -3,6 +3,8 @@
 from openharness.autopilot.service import RepoAutopilotStore
 from openharness.autopilot.types import (
     CronScheduleConfig,
+    PreflightCheck,
+    PreflightResult,
     RepoAutopilotRegistry,
     RepoJournalEntry,
     RepoRunResult,
@@ -14,6 +16,8 @@ from openharness.autopilot.types import (
 
 __all__ = [
     "CronScheduleConfig",
+    "PreflightCheck",
+    "PreflightResult",
     "RepoAutopilotRegistry",
     "RepoAutopilotStore",
     "RepoJournalEntry",
